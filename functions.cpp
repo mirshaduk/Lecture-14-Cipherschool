4d04200eb53a86c5a459d590c8f3547bfae1becd
mirshaduk <119918613+mirshaduk@users.noreply.github.com>
2024-07-08 10:07:53 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ 
+ void printstuff() //there are no parameters
+ {
+    cout<<"writing my first function";
+
+ }
+
+ int main()
+ {
+    printstuff();
+
+    return 0;
+
+ }
